fe6063a8a31d40c56369e286105128aebdca85de
Marcelo <57197501+marcelo-site@users.noreply.github.com>
2022-07-17 01:38:58 -0300

    landing-page-v0.1.1

--- js/js.js
+++ js/js.js
@@ -1,17 +1,17 @@
-    const menuClose = document.querySelector('#icon-close');
-    const menuOpen = document.querySelector('#icon-open');
-    const closeOpen = document.getElementById('close-menu')
-    function menu1(){
-        if( menuOpen.classList.length == 3){
-            closeOpen.classList.remove('visible-media');
-            menuOpen.classList.add('visible-media');
-            menuClose.classList.remove('visible-media')
-        } else if( menuOpen.classList.length == 4) {
-            closeOpen.classList.add('visible-media');
-            menuOpen.classList.remove('visible-media');
-            menuClose.classList.add('visible-media')
-        }
+const menuClose = document.querySelector('#icon-close');
+const menuOpen = document.querySelector('#icon-open');
+const closeOpen = document.getElementById('close-menu')
+function menu1(){
+    if( menuOpen.classList.length == 3){
+        closeOpen.classList.remove('visible-media');
+        menuOpen.classList.add('visible-media');
+        menuClose.classList.remove('visible-media')
+    } else if( menuOpen.classList.length == 4) {
+        closeOpen.classList.add('visible-media');
+        menuOpen.classList.remove('visible-media');
+        menuClose.classList.add('visible-media')
     }
+}
 const displayHeigth = window.innerHeight
 const topButton = document.querySelector('.back-to-top')
 let top1 = () => {
@@ -55,19 +55,20 @@ let srcValue = ''
 let src = ''
 let src1 = ''
 let src2 = ''
-let src3 = ''
-let src4 = ''
 const src5 = imgModal.length
+const body = document.querySelector('body')
+// const bodyModal=document.querySelector('#modal')
 function modal1() {
     for ( let index = 0;index < imgModal.length; index++) {
         imgModal[index].addEventListener ( 'click' , function() {
             srcValue = imgModal[index].getAttribute('src')
             divModal.setAttribute('src', srcValue)
             modal.classList.toggle('modal-active')
+            body.classList.add('body-overflow')
         })
     }
 }
-setaD.addEventListener ( 'click', () =>{
+setaD.addEventListener ( 'click', (e) =>{
     srcValue = divModal.getAttribute('src')
     src = srcValue.split('-')
     src1 = src[1].split ('.')
@@ -76,34 +77,36 @@ setaD.addEventListener ( 'click', () =>{
         divModal.setAttribute('src', src[0] +'-'+ (src2 + 1) + '.' + src1[1])
         if(src2 == (src5 - 1)) {
             setaD.classList.add('modal-fim')   
-        } 
-        else {
+        } else {
             setaE.classList.remove('modal-fim')
         }
     }
-    
-    // console.log(src2)
+    e.stopPropagation();
 })
-setaE.addEventListener ( 'click', () =>{
+setaE.addEventListener ( 'click', (e) =>{
     srcValue = divModal.getAttribute('src')
     src = srcValue.split('-')
-    src4 = src[1].split ('.')
-    src3 = parseInt(src4[0])
-    if (src3 > 1){
-        divModal.setAttribute('src', src[0] +'-'+ (src3 - 1) + '.' + src4[1])
-        if( src3 == 2){
+    src1 = src[1].split ('.')
+    src2 = parseInt(src1[0])
+    if (src2 > 1){
+        divModal.setAttribute('src', src[0] +'-'+ (src2 - 1) + '.' + src1[1])
+        if( src2 == 2){
             setaE.classList.add('modal-fim')   
-        }
-        else  {
-        setaD.classList.remove('modal-fim')
+        } else  {
+            setaD.classList.remove('modal-fim')
         }
     }
-  
-    // console.log(src3)
+    e.stopPropagation();
 })
 exitModal.addEventListener ( 'click' , function() {
     modal.classList.toggle('modal-active')
     setaD.classList.remove('modal-fim')
     setaE.classList.remove('modal-fim')
+    body.classList.remove('body-overflow')
 })
+modal.addEventListener('click', (e) => {
+    modal.classList.remove('modal-active')
+    body.classList.remove('body-overflow')
+    // e.stopPropagation();
+} )
 addEventListener ( 'load', modal1)
